6aea742ecf299a15ae1a877b5405526f8c2947ae
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-07-20 00:16:15 +0100

    Add test for user login, create article,and all the models

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,12 @@
 module ArticlesHelper
     def featured_article
-        @feature_article_id = Article.joins(:votes).group('article_id').count.first[0]
-        Article.find(@feature_article_id)
+        if Article.joins(:votes).group('article_id').count.first.nil?
+            flash[:notice] = "No Article available"
+        else
+            @feature_article_id = Article.joins(:votes).group('article_id').count.collect { |id, vote| [vote, id] }
+            feature_article_top = @feature_article_id.max[1]
+            Article.find(feature_article_top)
+        end
     end
 
 end
